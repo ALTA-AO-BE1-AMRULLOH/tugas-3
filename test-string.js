b0bacbdc24d414879388ac379f7192f3fa6955c5
amrulloh <amrullmisri@gmail.com>
2023-07-13 15:03:42 +0700

    first commit

---
+++ test-string.js
@@ -0,0 +1,8 @@
+let firstname = "Amrul"
+let lastname = "loh"
+let berthday = "1998"
+let nowyear = "2023"
+
+var fullname = firstname + lastname;
+var bornyear = bertday + nowyear
+console.log("hello,", fulnaem, "you are", bornyear, "years old",);
