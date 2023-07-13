6aa640508c93465de4ef2ea8ad9a3470e34ec066
amrulloh <amrullmisri@gmail.com>
2023-07-13 15:16:48 +0700

    commit

--- test-string.js
+++ test-string.js
@@ -3,6 +3,11 @@ let lastname = "loh"
 let berthday = "1998"
 let nowyear = "2023"
 
-var fullname = firstname + lastname;
-var bornyear = bertday + nowyear
-console.log("hello,", fulnaem, "you are", bornyear, "years old",);
+var fullname = firstname + lastname
+var bornyear = nowyear - berthday
+
+console.log("first name=", firstname)
+console.log("last name=", lastname)
+console.log("born year=", bornyear)
+
+console.log("hello,", fullname, "! you are", bornyear, "years old")
